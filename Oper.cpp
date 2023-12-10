0d148fd7e2d6a7d3497b4c01b0d876200d925d62
Ahmad Hawat <46716582+Ahmadhawat@users.noreply.github.com>
2023-12-10 21:25:12 +0100

    Update Oper.cpp

--- Oper.cpp
+++ Oper.cpp
@@ -1,4 +1,1 @@
 
-Hallo Michael,
-ich wollte dich informieren, dass ich am kommenden Montag nicht arbeiten werde. Am Mittwoch steht eine Zwischenprüfung an, und aufgrund eines Kunstseminars am Wochenende hatte ich keine Gelegenheit, mich darauf vorzubereiten. In dieser Woche werde ich am Donnerstag und Freitag von zu Hause aus arbeiten.
-Viele Grüße.
